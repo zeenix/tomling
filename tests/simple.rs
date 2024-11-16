31aeaa8f3262e49253dce11578a3b791e008d79b
Zeeshan Ali Khan <zeenix@gmail.com>
2024-11-16 15:21:47 +0100

    ✅ Convert the test into an actual test

--- tests/simple.rs
+++ tests/simple.rs
@@ -1,5 +1,7 @@
 #[test]
 fn simple_cargo_toml() {
+    use tomly::{parse, TomlMap, Value};
+
     let cargo_toml = r#"
         [package]
         name = "example"
@@ -13,13 +15,43 @@ fn simple_cargo_toml() {
         [features]
         default = ["serde"]
     "#;
+    let mut map = TomlMap::new();
+    map.insert(
+        "package",
+        Value::Table({
+            let mut package = TomlMap::new();
+            package.insert("name", Value::String("example"));
+            package.insert("version", Value::String("0.1.0"));
+            package.insert("edition", Value::String("2021"));
+            package
+        }),
+    );
+    map.insert(
+        "dependencies",
+        Value::Table({
+            let mut dependencies = TomlMap::new();
+            dependencies.insert(
+                "serde",
+                Value::Table({
+                    let mut serde = TomlMap::new();
+                    serde.insert("version", Value::String("1.0"));
+                    serde.insert("features", Value::Array(vec![Value::String("derive")]));
+                    serde
+                }),
+            );
+            dependencies.insert("regex", Value::String("1.5"));
+            dependencies
+        }),
+    );
+    map.insert(
+        "features",
+        Value::Table({
+            let mut features = TomlMap::new();
+            features.insert("default", Value::Array(vec![Value::String("serde")]));
+            features
+        }),
+    );
 
-    match tomly::parse(cargo_toml) {
-        Ok(map) => {
-            for (key, value) in map {
-                println!("{key}: {value:?}");
-            }
-        }
-        Err(err) => eprintln!("Failed to parse TOML: {err:?}"),
-    }
+    let parsed_map = parse(cargo_toml).unwrap();
+    assert_eq!(parsed_map, map);
 }
